fd7a9f100437317476d5374321f1501edc7c640d
Bastien <bgt.bastien@protonmail.com>
2025-10-14 09:39:48 +0200

    chore: initial project setup

---
+++ src/main/java/com/efrei/java/altn72_projet_bourgeat_caraux_guerin/Altn72ProjetBourgeatCarauxGuerinApplication.java
@@ -0,0 +1,13 @@
+package com.efrei.java.altn72_projet_bourgeat_caraux_guerin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Altn72ProjetBourgeatCarauxGuerinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Altn72ProjetBourgeatCarauxGuerinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/efrei/java/altn72_projet_bourgeat_caraux_guerin/Altn72ProjetBourgeatCarauxGuerinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efrei.java.altn72_projet_bourgeat_caraux_guerin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Altn72ProjetBourgeatCarauxGuerinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
