c24879615eb74398a38d07f930482d3c61239810
Nam <namguerin@gmail.com>
2025-10-29 23:25:58 +0100

    feat: add method to UserRepository for retrieving users by username, enhancing user management capabilities

--- src/main/java/com/efrei/java/altn72_projet_bourgeat_caraux_guerin/model/entities/repository/UserRepository.java
+++ src/main/java/com/efrei/java/altn72_projet_bourgeat_caraux_guerin/model/entities/repository/UserRepository.java
@@ -3,8 +3,17 @@ package com.efrei.java.altn72_projet_bourgeat_caraux_guerin.model.entities.repos
 import com.efrei.java.altn72_projet_bourgeat_caraux_guerin.model.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * Repository pour l'utilisateur
  */
 public interface UserRepository extends JpaRepository<User, Long> {
+    
+    /**
+     * Récupère un utilisateur par son nom d'utilisateur
+     * @param username Le nom d'utilisateur
+     * @return L'utilisateur si trouvé
+     */
+    Optional<User> findByUsername(String username);
 }
